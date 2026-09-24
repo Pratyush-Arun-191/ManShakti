b825221097c7fd24383014c07f26218f8a7f70df
Pratyush Arun <gpratyush619@gmail.com>
2026-09-24 20:04:23 +0530

    Allow deployed frontend access

--- backend/main.py
+++ backend/main.py
@@ -58,8 +58,8 @@ app = FastAPI(title="ManShakti API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False
     allow_methods=["*"],
     allow_headers=["*"],
 )
